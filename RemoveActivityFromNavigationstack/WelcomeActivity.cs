327fed873aca5ce2119989346372fb2982c86f59
mallibone <sirkenix@gmail.com>
2015-08-23 16:38:19 +0200

    Initial commit

---
+++ RemoveActivityFromNavigationstack/WelcomeActivity.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace RemoveActivityFromNavigationstack
+{
+    [Activity(Label = "RemoveActivityFromNavigationstack", MainLauncher = true, Icon = "@drawable/icon")]
+    public class WelcomeActivity : Activity
+    {
+        protected override async void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Welcome);
+
+            // Do some meaninthing stuff here, or just wait for 3 seconds...
+            await Task.Delay(3000);
+
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.SetFlags(ActivityFlags.NewTask);
+            StartActivity(intent);
+            Finish();
+        }
+    }
+}
